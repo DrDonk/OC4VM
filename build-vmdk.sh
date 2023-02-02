171301e04d4f5348ae742f4423ba00e84143bab1
David Parsons <dave@daveparsons.net>
2023-02-02 12:25:06 +0000

    Split config.plist generation out into separate shell script

--- build-vmdk.sh
+++ build-vmdk.sh
@@ -41,25 +41,13 @@ msg_error() {
 }
 
 # Clear previous build
-rm -rfv ./Config/*
 rm -rfv ./VMDK/*
 
 # Create new output folders
-mkdir -p ./Config/release_intel
-mkdir -p ./Config/release_amd
-mkdir -p ./Config/debug_intel
-mkdir -p ./Config/debug_amd
-
 mkdir -p ./VMDK/release_intel
 mkdir -p ./VMDK/release_amd
 mkdir -p ./VMDK/debug_intel
 mkdir -p ./VMDK/debug_amd
-
-# Build config.plist files
-jinja2 --format=toml --section=release_intel --outfile=./Config/release_intel/config.plist config.j2 config.toml
-jinja2 --format=toml --section=release_amd --outfile=./Config/release_amd/config.plist config.j2 config.toml
-jinja2 --format=toml --section=debug_intel --outfile=./Config/debug_intel/config.plist config.j2 config.toml
-jinja2 --format=toml --section=debug_amd --outfile=./Config/debug_amd/config.plist config.j2 config.toml
 
 # Build the OpenCore DMG/VMDK files
 MSG="Intel Release"
