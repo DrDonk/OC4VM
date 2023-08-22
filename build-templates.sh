c54d38fc30888dc292cb98d3fbf7adca9fd26c38
David Parsons <dave@daveparsons.net>
2023-08-22 17:49:28 +0100

    Remove all AMD code as it does not work

--- build-templates.sh
+++ build-templates.sh
@@ -13,24 +13,6 @@ rm -rf ./build/templates/* 2>/dev/null
 # Build VMX files
 mkdir -p ./build/templates/intel
 cp -v macos.vmdk ./build/templates/intel
-cp -v ./build/vmdk/intel-release/opencore.* ./build/templates/intel
-
-mkdir -p ./build/templates/amd
-cp -v macos.vmdk ./build/templates/amd
-cp -v ./build/vmdk/amd-release/opencore.* ./build/templates/amd
-
+cp -v ./build/vmdk/intel-release/opencore.vmdk ./build/templates/intel
 jinja2 --format=toml --section=intel_generic -D VERSION=$VERSION --outfile=./build/templates/intel/macos.vmx vmx.j2 vmx.toml
-#jinja2 --format=toml --section=intel_macos_10_15 -D VERSION=$VERSION --outfile=./build/templates/intel/macos1015.vmx vmx.j2 vmx.toml
-#jinja2 --format=toml --section=intel_macos_11 -D VERSION=$VERSION --outfile=./build/templates/intel/macos11.vmx vmx.j2 vmx.toml
-#jinja2 --format=toml --section=intel_macos_12 -D VERSION=$VERSION --outfile=./build/templates/intel/macos12.vmx vmx.j2 vmx.toml
-#jinja2 --format=toml --section=intel_macos_13 -D VERSION=$VERSION --outfile=./build/templates/intel/macos13.vmx vmx.j2 vmx.toml
-#jinja2 --format=toml --section=intel_macos_14 -D VERSION=$VERSION --outfile=./build/templates/intel/macos14.vmx vmx.j2 vmx.toml
-
-jinja2 --format=toml --section=amd_generic -D VERSION=$VERSION --outfile=./build/templates/amd/macos.vmx vmx.j2 vmx.toml
-#jinja2 --format=toml --section=amd_macos_10_15 -D VERSION=$VERSION --outfile=./build/templates/amd/macos1015.vmx vmx.j2 vmx.toml
-#jinja2 --format=toml --section=amd_macos_11 -D VERSION=$VERSION --outfile=./build/templates/amd/macos11.vmx vmx.j2 vmx.toml
-#jinja2 --format=toml --section=amd_macos_12 -D VERSION=$VERSION --outfile=./build/templates/amd/macos12.vmx vmx.j2 vmx.toml
-#jinja2 --format=toml --section=amd_macos_13 -D VERSION=$VERSION --outfile=./build/templates/amd/macos13.vmx vmx.j2 vmx.toml
-#jinja2 --format=toml --section=amd_macos_14 -D VERSION=$VERSION --outfile=./build/templates/amd/macos14.vmx vmx.j2 vmx.toml
-
 exit
