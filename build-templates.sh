2347868cb4913b4a271285b95a5a2ca752fc5561
David Parsons <dave@daveparsons.net>
2023-10-02 18:22:06 +0100

    Fix build errors for templates

--- build-templates.sh
+++ build-templates.sh
@@ -13,6 +13,6 @@ rm -rf ./build/templates/* 2>/dev/null
 # Build VMX files
 mkdir -p ./build/templates/vmware
 cp -v macos.vmdk ./build/templates/vmware
-cp -v ./build/vmdk/intel-release/opencore.vmdk ./build/templates/intel
-jinja2 --format=toml --section=intel_generic -D VERSION=$VERSION --outfile=./build/templates/vmware/macos.vmx vmx.j2 vmx.toml
+cp -v ./build/vmdk/intel-release/opencore.vmdk ./build/templates/vmware
+jinja2 --format=toml --section=intel-release -D VERSION=$VERSION --outfile=./build/templates/vmware/macos.vmx vmx.j2 vmx.toml
 exit
